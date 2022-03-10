0068a929396160342eed93db26a11ebf8fbdaf52
christhomp13 <theinfinitehumanblueprint@gmail.com>
2022-03-09 21:49:29 -0500

    update

--- components/Header.js
+++ components/Header.js
@@ -7,12 +7,12 @@ function Header() {
   return (
     <div className='top-0 flex justify-between text-xl text-white'>
         <div className='flex p-5 space-x-5'>
-            <h2 onClick={()=> router.push('https://about.google/')}className='cursor-pointer hover:underline'>About</h2>
-            <h2 onClick={()=> router.push('https://store.google.com/US')} className='cursor-pointer hover:underline'>Store</h2>
+            <p onClick={()=> router.push('https://about.google/')}className='cursor-pointer hover:underline'>About</p>
+            <p onClick={()=> router.push('https://store.google.com/US')} className='cursor-pointer hover:underline'>Store</p>
         </div>
         <div className='flex p-5 space-x-5'>
-            <h2 onClick={()=> router.push('https://www.gmail.com')}className='cursor-pointer hover:underline' >Gmail</h2>
-            <h2 onClick={()=> router.push('https://www.google.com/imghp?hl=en&authuser=0&ogbl')}className='cursor-pointer hover:underline'>Images</h2>
+            <p onClick={()=> router.push('https://www.gmail.com')}className='cursor-pointer hover:underline' >Gmail</p>
+            <p onClick={()=> router.push('https://www.google.com/imghp?hl=en&authuser=0&ogbl')}className='cursor-pointer hover:underline'>Images</p>
             <div className='flex rounded-full hover:bg-neutral-500 p-1 cursor-pointer'>
                 <GlobeAltIcon className="h-6 w-6 text-white"/>
                 
